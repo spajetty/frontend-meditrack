d5879c595ecf7c41e1f0fc1604d3edf83d915def
Jetty Aguilar <jethrojamesaguilar.18@gmail.com>
2025-06-20 23:58:18 +0800

    removed unneccsarry comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingLayout from "./layouts/LandingLayout";
 import LandingPage from "./pages/LandingPage";
-import LoginPage from "./pages/LoginPage";     // Create this later
-import RegisterPage from "./pages/RegisterPage"; // Create this later
+import LoginPage from "./pages/LoginPage";     
+import RegisterPage from "./pages/RegisterPage"; 
 
 export default function App() {
   return (
