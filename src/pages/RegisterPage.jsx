e2f47137c1504774ed29357f2c0b2ed0ab6bab37
Jetty Aguilar <jethrojamesaguilar.18@gmail.com>
2025-06-21 17:24:32 +0800

    changed bg color

--- src/pages/RegisterPage.jsx
+++ src/pages/RegisterPage.jsx
@@ -16,7 +16,7 @@ export default function RegisterPage() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-4">
+    <div className="min-h-screen flex items-center justify-center bg-dark-teal p-4">
       <div className="bg-white p-8 rounded shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-4 text-center">Welcome to MediTrack</h2>
 
