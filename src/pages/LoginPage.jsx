81023bb872e565d7f014ec50ad67be297630bd60
Jetty Aguilar <jethrojamesaguilar.18@gmail.com>
2025-06-21 19:37:42 +0800

    removed apos

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -64,7 +64,7 @@ export default function LoginPage() {
         </form>
 
         <p className="mt-4 text-center text-sm text-gray-600">
-          Don&apos;t have an account?{" "}
+          Don't have an account?{" "}
           <Link
             to="/register"
             className="text-blue-500 font-medium hover:underline"
