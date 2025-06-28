26abee0f66b3f3dd14db39593dd82068fe2d45f5
Jetty Aguilar <jethrojamesaguilar.18@gmail.com>
2025-06-28 16:01:02 +0800

    created status column

--- src/pages/Prescription.jsx
+++ src/pages/Prescription.jsx
@@ -60,6 +60,7 @@ export default function Prescription() {
               <th className="px-4 py-2">Dosage</th>
               <th className="px-4 py-2">Times</th>
               <th className="px-4 py-2">Days</th>
+              <th className="px-4 py-2">Status</th>
               <th className="px-4 py-2">Action</th>
             </tr>
           </thead>
