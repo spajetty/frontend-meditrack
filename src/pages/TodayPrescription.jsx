51749e6f0e264f1a66ec57746b7f81b84fcc1763
Jetty Aguilar <jethrojamesaguilar.18@gmail.com>
2025-06-29 23:39:19 +0800

    recolored the buttons

--- src/pages/TodayPrescription.jsx
+++ src/pages/TodayPrescription.jsx
@@ -116,7 +116,7 @@ export default function TodayPrescription() {
                   <td className="px-4 py-2 text-center space-y-1">
                     {log.status === 0 && (
                       <button
-                        className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600 transition w-39"
+                        className="bg-green-700 text-white px-3 py-1 rounded hover:bg-green-500 transition w-39"
                         onClick={() => markAsTaken(log.doseLogId)}
                       >
                         Mark as Taken
@@ -132,7 +132,7 @@ export default function TodayPrescription() {
                     )}
                     {log.status === 1 && (
                       <button
-                        className="bg-yellow-500 text-white px-3 py-1 rounded hover:bg-yellow-600 transition w-39"
+                        className="bg-yellow-700 text-white px-3 py-1 rounded hover:bg-yellow-500 transition w-39"
                         onClick={() => undoDose(log.doseLogId)}
                       >
                         Undo Action
