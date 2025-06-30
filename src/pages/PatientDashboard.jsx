e5673818f2b8b52ee3896bc45ac9bad8edf315a5
Jetty Aguilar <jethrojamesaguilar.18@gmail.com>
2025-06-30 21:39:29 +0800

    made the buttons

--- src/pages/PatientDashboard.jsx
+++ src/pages/PatientDashboard.jsx
@@ -13,6 +13,26 @@ export default function PatientDashboard() {
       </div>
       <div>
         <img src={banner} alt="" className="lg:w-xl md:w-lg sm:w-lg w-sm"/>
+      </div><br />
+      <div className="flex flex-wrap gap-4 mb-4">
+        <button
+          onClick={() => navigate("/today-prescriptions")}
+          className="bg-emerald-500 hover:bg-emerald-600 text-white px-5 py-2 rounded-md transition"
+        >
+          Today's Prescriptions
+        </button>
+        <button
+          onClick={() => navigate("/prescriptions")}
+          className="bg-blue-500 hover:bg-blue-600 text-white px-5 py-2 rounded-md transition"
+        >
+          All Prescriptions
+        </button>
+        <button
+          onClick={() => navigate("/history")}
+          className="bg-purple-500 hover:bg-purple-600 text-white px-5 py-2 rounded-md transition"
+        >
+          How to Use
+        </button>
       </div>
     </div>
   );
